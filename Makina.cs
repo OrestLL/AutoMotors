97c3065c100048271a271a86d0500d05dc008a87
HP <HP@DESKTOP-8EJ06EC>
2023-07-16 10:58:05 +0200

    Add project files.

---
+++ Makina.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMotors
+{
+    public class Makina
+    {
+        public string Marka;
+        public string Modeli;
+        public int Viti;
+        public string Ngjyra;
+        public double Fuqia;
+        public double Kapaciteti;
+        public double KapacitetiTotal;
+        public string Karburant;
+        public double CmimiEuro;
+        public double Harxhimi;
+
+
+        public Makina(string marka, string modeli, string ngjyra, int viti, double fuqia, double kapaciteti, double kapacitetiTotal, string karburant, double harxhimi, double cmimiEuro) 
+        {
+            Marka = marka;
+            Modeli = modeli;
+            Ngjyra = ngjyra;
+            Viti = viti;
+            Fuqia = fuqia;
+            Kapaciteti = kapaciteti;
+            KapacitetiTotal = kapacitetiTotal;
+            Karburant = karburant;
+            Harxhimi = harxhimi;
+            CmimiEuro = cmimiEuro;
+        }
+
+        public void ShfaqOpsionet()
+        {
+            Console.WriteLine("1. Bej Xhiro     2. Laj Makinen      3. Parko        4. Furnizo          0. Dil");
+        }
+        public void Specifikimi(Makina makina)
+        {
+            Console.WriteLine($"Ju zgjodhet {makina.Marka} {makina.Modeli} . Parametrat e makines jane keto:");
+            Console.WriteLine($"Viti i Prodhimit: {makina.Viti}");
+            Console.WriteLine($"Lloji i Karburantit: {makina.Karburant}");
+            Console.WriteLine($"Motorri: {makina.Fuqia}");
+        }
+
+        public void Xhiro(Makina makina)
+
+        {
+            Console.WriteLine("Ti po ben nje xhiro!");
+            Random rnd = new Random();
+            int km = rnd.Next(1, 200);
+
+
+            double l = km * makina.Harxhimi;
+            if (makina.Kapaciteti > l)
+            {
+                makina.Kapaciteti -= l;
+
+                double cmimi = Cmimi (makina.Karburant) * l;
+
+
+                Console.WriteLine($"Ke bere {km} kilometra dhe jane kosumuar {Math.Round(l, 2)} litra nafte!");
+                Console.WriteLine($"Per kete udhetim jane shpenzuar {Math.Round(cmimi, 2)} LEK");
+                Console.WriteLine($"Kane mbetur {Math.Round(makina.Kapaciteti, 2)} litra");
+            }
+
+
+            else
+            {
+                Console.WriteLine("Furnizo");
+            }
+        }
+        public void Parko()
+        {
+            Console.WriteLine(" Ti parkove makinen!");
+        }
+        public void Laje()
+        {
+            Console.WriteLine(" Ti e fute ne lavazh per te lare makinen!");
+        }
+        public void Furnizo(Makina makina)
+        {
+            Console.WriteLine("Sa lek do ta furnizoni mjetin mjetin tuaj?");
+            double lek = Convert.ToInt32(Console.ReadLine());
+
+            double l = lek / Cmimi(makina.Karburant);
+
+            if (makina.Kapaciteti < makina.KapacitetiTotal)
+            {
+                makina.Kapaciteti += l;
+                if (makina.Kapaciteti > makina.KapacitetiTotal)
+                {
+                    double teprica = makina.Kapaciteti - makina.KapacitetiTotal;
+                    double kusuri = teprica * Cmimi(makina.Karburant);
+                    makina.Kapaciteti = makina.KapacitetiTotal;
+
+                    double km = makina.Kapaciteti / makina.Harxhimi;
+                    double Furnizimi = (lek - kusuri) / Cmimi(makina.Karburant);
+
+
+                    Console.WriteLine($"U shtuan {Math.Round(Furnizimi, 2)} litra nafte!");
+                    Console.WriteLine($"kusuri: {Math.Round(kusuri, 2)} Lek!");
+                    Console.WriteLine($"{Math.Round(makina.Kapaciteti, 2)} litra te mbetura!");
+                    Console.WriteLine($"{Math.Round(km)} kilometra te mbetura!");
+                }
+                else
+                {
+                    double km = makina.Kapaciteti / makina.Harxhimi;
+
+
+                    Console.WriteLine($"U shtuan {Math.Round(l, 2)} litra nafte!");
+                    Console.WriteLine($"{Math.Round(makina.Kapaciteti, 2)} litra te mbetura!");
+                    Console.WriteLine($"{Math.Round(km)} kilometra te mbetura!");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Makina eshte e furnizuar");
+            }
+            Console.WriteLine(" Ti furnizove me karburant makinen!");
+        }
+        private int Cmimi (string lloji)
+        {
+            int cmimi = lloji == "Nafte" ? 176 : 178;
+            return cmimi;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,177 @@
+﻿using AutoMotors;
+
+Makina Audi = new Makina("Audi Q4","Audi Q4 E-Tronic", "Bardhe", 2023, 3.0, 75, 75, "Nafte", 0.047, 55000);
+Makina Mercedes = new Makina("Mercedes-Benz", "W203", "I zi", 2006, 2.2, 66, 66, "Nafte", 0.134, 5000);
+Makina BMW = new Makina("BMW Seria 5", "W203", "I zi", 2006, 2.2, 57, 57, "Nafte", 0.134, 56000);
+
+
+Console.WriteLine("Mire se vini ne Auto Motors!");
+Console.WriteLine(" Ju lutem zgjidhni opsionin e makines tuaj!");
+
+Console.WriteLine($"1.{Audi.Marka} {Audi.Modeli}    2. {Mercedes.Marka} {Mercedes.Modeli}   3.{BMW.Marka} {BMW.Modeli}");
+int opsioni = Convert.ToInt32(Console.ReadLine());
+if (opsioni == 1)
+{
+    Audi.Specifikimi(Audi);
+
+    while (true)
+    {
+        Audi.ShfaqOpsionet();
+        int op = Convert.ToInt32(Console.ReadLine());
+
+        if (op == 1)
+        {
+            Audi.Xhiro(Audi);
+         
+        }
+        else if (op == 2)
+        {
+            Audi.Laje();
+            continue;
+        }
+        else if (op == 3)
+        {
+            Audi.Parko();
+            continue;
+        }
+        else if (op == 4)
+
+        {
+            Audi.Furnizo(Audi);
+        }
+        else if (op == 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Zgjedhje eshte e gabuar!");
+            
+        }
+    }
+
+}
+else if (opsioni == 2)
+{
+
+    BMW.Specifikimi(BMW);
+
+    while (true)
+    {
+        BMW.ShfaqOpsionet();
+        int op = Convert.ToInt32(Console.ReadLine());
+    }
+        Console.WriteLine($"Ju zgjodhet {BMW.Marka} {BMW.Modeli}. Parametrat e makines jane keto:");
+    Console.WriteLine($"Viti i Prodhimit: {BMW.Viti}");
+    Console.WriteLine($"Lloji i Karburantit: {BMW.Karburant}");
+    Console.WriteLine($"Motorri: {BMW.Fuqia}");
+
+    while (true)
+    {
+        Console.WriteLine("1. Bej Xhiro     2. Laj Makinen      3. Parko        4. Furnizo          0. Dil");
+        int op = Convert.ToInt32(Console.ReadLine());
+
+        if (op == 1)
+        {
+            Random rnd = new Random();
+            int km = rnd.Next(1, 200);
+
+            BMW.Xhiro(BMW);
+
+            // BMW harxhon 9.6l - 100 km => 0.096l - 1km
+            // Cmimi aktual i benzines = 179 LEK / l
+
+            double l = km * 0.096;
+            double cmimi = 179 * l;
+
+            Console.WriteLine($"Ke bere {km} kilometra dhe jane kosumuar {Math.Round(l, 2)} litra nafte!");
+            Console.WriteLine($"Per kete udhetim jane shpenzuar {Math.Round(cmimi, 2)} LEK");
+
+            continue;
+        }
+        else if (op == 2)
+        {
+            BMW.Laje();
+            continue;
+        }
+        else if (op == 3)
+        {
+            BMW.Parko();
+            continue;
+        }
+        else if (op == 4)
+        {
+            BMW.Furnizo(BMW);
+            continue;
+        }
+        else if (op == 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Zgjedhje eshte e gabuar!");
+            continue;
+        }
+    }
+}
+else if (opsioni == 3)
+{
+    Console.WriteLine($"Ju zgjodhet {Mercedes.Marka} {Mercedes.Modeli}. Parametrat e makines jane keto:");
+    Console.WriteLine($"Viti i Prodhimit: {Mercedes.Viti}");
+    Console.WriteLine($"Lloji i Karburantit: {Mercedes.Karburant}");
+    Console.WriteLine($"Motorri: {Mercedes.Fuqia}");
+
+    while (true)
+    {
+        Console.WriteLine("1. Bej Xhiro     2. Laj Makinen      3. Parko        4. Furnizo          0. Dil");
+        int op = Convert.ToInt32(Console.ReadLine());
+
+        if (op == 1)
+        {
+            Random rnd = new Random();
+            int km = rnd.Next(1, 200);
+
+            Mercedes.Xhiro(Mercedes);
+
+            // Mercedes harxhon 13.4l - 100 km => 0.134l - 1km
+            // Cmimi aktual i benzines = 179 LEK / l
+
+            double l = km * 0.134;
+            double cmimi = 179 * l;
+
+            Console.WriteLine($"Ke bere {km} kilometra dhe jane kosumuar {Math.Round(l, 2)} litra nafte!");
+            Console.WriteLine($"Per kete udhetim jane shpenzuar {Math.Round(cmimi, 2)} LEK");
+
+            continue;
+        }
+        else if (op == 2)
+        {
+            Mercedes.Laje();
+            continue;
+        }
+        else if (op == 3)
+        {
+            Mercedes.Parko();
+            continue;
+        }
+        else if (op == 4)
+        {
+            Mercedes.Furnizo(Mercedes);
+            continue;
+        }
+        else if (op == 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Zgjedhje eshte e gabuar!");
+            continue;
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Zgjedhja eshte e gabuar!");
+}
